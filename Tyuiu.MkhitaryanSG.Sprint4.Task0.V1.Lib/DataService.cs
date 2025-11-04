2b2b9d2327a252307a9733597a8aba1006c3f626
София Мхитарян <sofiya.mkhitaryan.04@mail.ru>
2025-11-04 16:53:01 +0500

    Add project files.

---
+++ Tyuiu.MkhitaryanSG.Sprint4.Task0.V1.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.MkhitaryanSG.Sprint4.Task0.V1.Lib
+{
+    public class DataService : ISprint4Task0V1
+    {
+        public int GetSumEvenArrEl(int[] array)
+        {
+            int sum = 0;
+            foreach (int number in array)
+            {
+                if (number % 2 == 0) // Проверка на четность
+                {
+                    sum += number; // Суммируем четные элементы
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.MkhitaryanSG.Sprint4.Task0.V1.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MkhitaryanSG.Sprint4.Task0.V1.Lib;
+namespace Tyuiu.MkhitaryanSG.Sprint4.Task0.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[] nums = {6, 4, 3, 2, 1, 0, 9, 8, 7, 5};
+            int res = ds.GetSumEvenArrEl(nums);
+            int wait = 20;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
