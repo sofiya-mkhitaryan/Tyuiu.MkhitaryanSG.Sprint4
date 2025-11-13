ccbd45cba184319439cda2e1531e8a884275b8ba
София Мхитарян <sofiya.mkhitaryan.04@mail.ru>
2025-11-13 11:15:50 +0500

    Task3

---
+++ Tyuiu.MkhitaryanSG.Sprint4.Task3.V27.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.MkhitaryanSG.Sprint4.Task3.V27.Lib
+{
+    public class DataService : ISprint4Task3V27
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetUpperBound(0) + 1; //кол=во строк
+            int columns = array.Length / rows; //кол=во столбцов
+            int count = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] % 2 == 0) 
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count; 
+        }
+    }
+}
---
+++ Tyuiu.MkhitaryanSG.Sprint4.Task3.V27.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.MkhitaryanSG.Sprint4.Task3.V27.Lib;
+namespace Tyuiu.MkhitaryanSG.Sprint4.Task3.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[5, 5] { { 4, 3, 5, 5, 3 },
+                                          { 6, 7, 4, 4, 3 },
+                                          { 3, 3, 7, 3, 6 },
+                                          { 3, 4, 3, 7, 7 },
+                                          { 3, 5, 6, 3, 6 } };
+            int res = ds.Calculate(mas2);
+            int wait = 8;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
