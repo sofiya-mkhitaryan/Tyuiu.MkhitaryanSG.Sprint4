35456689031a309efd570f981399c49d9e4354f2
София Мхитарян <sofiya.mkhitaryan.04@mail.ru>
2025-11-15 12:12:55 +0500

    Task7

--- Tyuiu.MkhitaryanSG.Sprint4.Task7.V4.Lib/DataService.cs
+++ Tyuiu.MkhitaryanSG.Sprint4.Task7.V4.Lib/DataService.cs
@@ -11,7 +11,9 @@ namespace Tyuiu.MkhitaryanSG.Sprint4.Task7.V4.Lib
             {
                 for (int j = 0; j < m; j++)
                 {
-                    matrx[i, j] = int.Parse(value.Substring(i * m + j, 1));
+                    //matrx[i, j] = int.Parse(value.Substring(i * m + j, 1));
+                    matrx[i, j] = int.Parse(value.Substring(index, 1));
+                    index++;
                 }
             }
 
